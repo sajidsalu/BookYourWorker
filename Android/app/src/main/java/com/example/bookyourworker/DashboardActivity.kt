b1e26705aee45035db205b0e804c813376416736
sajid <sajid.m@experionglobal.com>
2020-05-30 01:00:35 +0530

    #splash activity

--- Android/app/src/main/java/com/example/bookyourworker/DashboardActivity.kt
+++ Android/app/src/main/java/com/example/bookyourworker/DashboardActivity.kt
@@ -1,10 +1,10 @@
 package com.example.bookyourworker
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
 
-class MainActivity : AppCompatActivity() {
-
+class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ Android/app/src/main/java/com/example/bookyourworker/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.example.bookyourworker
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private val mHandler = Handler()
+    private val mHandlerProgress = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_splash)
+
+        mHandlerProgress.postDelayed(object : Runnable {
+            override fun run() {
+                showProgress()
+            }
+
+        }, 1000)
+        mHandler.postDelayed(object : Runnable {
+            override fun run() {
+                navigate()
+            }
+
+        }, 4000)
+    }
+
+    fun showProgress() {
+        simpleProgressBar.visibility = View.VISIBLE
+    }
+
+    fun navigate() {
+        val intent = Intent(this, DashboardActivity::class.java).apply {
+            //putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+        finish();
+    }
+}
+
